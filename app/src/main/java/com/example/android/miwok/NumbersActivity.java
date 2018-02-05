adb317925ccd68e4345c7cb2c53a007600f1a266
bruno78 <devbruno78@gmail.com>
2018-02-05 00:00:33 -0500

    Add custom layout

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,39 +3,37 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
-import android.widget.GridView;
-import android.widget.LinearLayout;
+
 import android.widget.ListView;
-import android.widget.TextView;
+
 
 import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
-    private ArrayList<String> words;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        words = new ArrayList<String>();
-        words.add("one");
-        words.add("two");
-        words.add("three");
-        words.add("four");
-        words.add("five");
-        words.add("six");
-        words.add("seven");
-        words.add("eight");
-        words.add("nine");
-        words.add("ten");
+        ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("one", "lutti"));
+        words.add(new Word("two", "otiiko"));
+        words.add(new Word("three", "tolookosu"));
+        words.add(new Word("four", "oyyisa"));
+        words.add(new Word("five", "massoka"));
+        words.add(new Word("six", "temmokka"));
+        words.add(new Word("seven", "kenekaku"));
+        words.add(new Word("eight", "kawinta"));
+        words.add(new Word("nine", "wo'e"));
+        words.add(new Word("ten", "na'aacha"));
 
-        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, R.layout.list_item, words);
+        WordAdapter wordAdapter = new WordAdapter(this, words);
 
         ListView listView = (ListView) findViewById(R.id.list);
 
-        listView.setAdapter(itemsAdapter);
+        listView.setAdapter(wordAdapter);
 
 
     }
--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -10,12 +10,12 @@ public class Word {
 
     /**
      * This constructor accepts two parameters, first is the Miwok word and second is the translation
-     * @param mMiwokTranslation is Miwork word to be translated
-     * @param mDefaultTranslation is the translation of the Miwok word
+     * @param miwokWord to be translated
+     * @param defaultWord is the translation of the Miwok word
      */
-    public Word(String mDefaultTranslation, String mMiwokTranslation) {
-        mMiwokTranslation = mMiwokTranslation;
-        mDefaultTranslation = mDefaultTranslation;
+    public Word(String defaultWord, String miwokWord) {
+        mMiwokTranslation = miwokWord;
+        mDefaultTranslation = defaultWord;
     }
 
     /**
@@ -30,24 +30,6 @@ public class Word {
      */
     public String getMiwokTranslation(){
         return mMiwokTranslation;
-    }
-
-    /**
-     * This method translates English word into Miwok
-     * @param defaultTranslation to be translated into Miwok language
-     * @return Miwok translation
-     */
-    public String getMiwokTranslation(String defaultTranslation) {
-        if(mDefaultTranslation.toLowerCase().equals(mDefaultTranslation)) return mMiwokTranslation;
-    }
-
-    /**
-     * This method tranlates Miwok word into English
-     * @param miwokTranslation to be translated into English language
-     * @return English translation
-     */
-    public String getEnglishTranslation(String miwokTranslation) {
-        if(mMiwokTranslation.toLowerCase().equals(mMiwokTranslation)) return mDefaultTranslation
     }
 
 }
